91acc58fa6e875f133513e7c9863382c12ee1f45
Himanshi Bishnoi <himanshibishnoi.work@gmail.com>
2026-06-22 14:24:28 +0530

    Add cycle detection and DFS-based topological sort

--- backend/graph.py
+++ backend/graph.py
@@ -11,24 +11,67 @@ class Graph:
     def print_graph(self):
         for node, neighbors in self.graph.items():
             print(f"{node} -> {neighbors}")
+
     def get_in_degrees(self):
         in_degrees = {node: 0 for node in self.graph}
         for node in self.graph:
             for neighbor in self.graph[node]:
                 in_degrees[neighbor] += 1
         return in_degrees
+
     def topological_sort_kahn(self):
         in_degrees = self.get_in_degrees()
         queue = [node for node in in_degrees if in_degrees[node] == 0]
         result = []
         while queue:
-            node = queue.pop(0) 
+            node = queue.pop(0)
             result.append(node)
             for neighbor in self.graph[node]:
                 in_degrees[neighbor] -= 1
                 if in_degrees[neighbor] == 0:
                     queue.append(neighbor)
         return result
+
+    def has_cycle(self):
+        visited = set()
+        in_stack = set()
+
+        def dfs(node):
+            visited.add(node)
+            in_stack.add(node)
+            for neighbor in self.graph[node]:
+                if neighbor in in_stack:
+                    return True
+                if neighbor not in visited:
+                    if dfs(neighbor):
+                        return True
+            in_stack.remove(node)
+            return False
+
+        for node in self.graph:
+            if node not in visited:
+                if dfs(node):
+                    return True
+        return False
+    def topological_sort_dfs(self):
+        visited = set()
+        result = []
+
+        def dfs(node):
+            visited.add(node)
+            for neighbor in self.graph[node]:
+                if neighbor not in visited:
+                    dfs(neighbor)
+            result.append(node)
+
+        for node in self.graph:
+            if node not in visited:
+                dfs(node)
+
+        return result[::-1]
+
+
+# Test code
 my_graph = Graph()
 my_graph.add_node("Source Code")
 my_graph.add_node("Object Code")
@@ -36,7 +79,6 @@ my_graph.add_node("Linking")
 my_graph.add_node("Compiling")
 my_graph.add_node("Testing")
 my_graph.add_node("Documentation")
-
 my_graph.add_edge("Source Code", "Object Code")
 my_graph.add_edge("Object Code", "Linking")
 my_graph.add_edge("Linking", "Compiling")
@@ -44,6 +86,15 @@ my_graph.add_edge("Compiling", "Testing")
 my_graph.add_edge("Source Code", "Documentation")
 
 my_graph.print_graph()
+print(my_graph.topological_sort_kahn())
+print(my_graph.has_cycle())
 
-order = my_graph.topological_sort_kahn()
-print(order)
+cyclic_graph = Graph()
+cyclic_graph.add_node("A")
+cyclic_graph.add_node("B")
+cyclic_graph.add_node("C")
+cyclic_graph.add_edge("A", "B")
+cyclic_graph.add_edge("B", "C")
+cyclic_graph.add_edge("C", "A")
+print(cyclic_graph.has_cycle())
+print(my_graph.topological_sort_dfs())
